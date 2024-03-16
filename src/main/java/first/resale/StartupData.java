e0c1a6e64d4e06993445cfdd21ba43d1ce23faa8
Y pc <yazamail@yandex.ru>
2024-03-16 13:03:27 +0300

    admin and user added

--- src/main/java/first/resale/StartupData.java
+++ src/main/java/first/resale/StartupData.java
@@ -2,28 +2,67 @@ package first.resale;
 
 import first.resale.models.Currency;
 import first.resale.models.Product;
+import first.resale.models.Role;
+import first.resale.models.User;
 import first.resale.service.ProductService;
+import first.resale.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.xml.crypto.Data;
 import java.math.BigDecimal;
+import java.sql.Date;
 import java.sql.Timestamp;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.DateTime;
 
 @Component
 public class StartupData implements CommandLineRunner {
+    private final UserService userService;
     private final ProductService productService;
 
     @Autowired
-    public StartupData(ProductService productService) {
+    public StartupData(UserService userService, ProductService productService) {
+        this.userService = userService;
         this.productService = productService;
     }
 
     @Override
     public void run(String... args) {
+        adminAccount();
+        userAccount();
         exampleProducts();
+    }
+
+    private void userAccount(){
+        User user = new User();
+
+        user.setName("user");
+        user.setPatronymic("userPatronymic");
+        user.setSurname("userSurname");
+        user.setBirthDay(new Date(88, 2, 15));
+        user.setPhoneNumber("777");
+        user.setStatus(Role.ROLE_USER.toString());
+        user.setPassword("user");
+
+        userService.save(user);
+    }
+
+    private void adminAccount(){
+        User admin = new User();
+
+        admin.setName("admin");
+        admin.setPatronymic("userAdminPatronymic");
+        admin.setSurname("userSurname");
+        admin.setBirthDay(new Date(98, 2, 15));
+        admin.setPhoneNumber("777");
+        admin.setStatus(Role.ROLE_USER.toString());
+        admin.setPassword("admin");
+
+        userService.save(admin);
     }
 
     private void exampleProducts(){
--- src/main/java/first/resale/models/User.java
+++ src/main/java/first/resale/models/User.java
@@ -1,23 +1,44 @@
 package first.resale.models;
 
-import javax.xml.crypto.Data;
+import com.sun.istack.NotNull;
+import jakarta.persistence.*;
 
-//@Entity
-//@Table(name = "t_user")
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @Column(name = "name")
+    @NotNull
     private String name;
+    @Column(name = "patronymic")
+    @NotNull
     private String patronymic;
+    @Column(name = "surname")
+    @NotNull
     private String surname;
-    private Data birthDay;
+    @Column(name = "birthDay")
+    @NotNull
+    private Date birthDay;
+    @Column(name = "phoneNumber")
+    @NotNull
     private String phoneNumber;
+    @Column(name = "status")
+    @NotNull
     private String status;
+    @Column(name = "password")
+    @NotNull
     private String password;
 
     public User() {
     }
 
-    public User(long id, String name, String patronymic, String surname, Data birthDay, String phoneNumber, String status, String password) {
+    public User(long id, String name, String patronymic, String surname, Date birthDay, String phoneNumber, String status, String password) {
         this.id = id;
         this.name = name;
         this.patronymic = patronymic;
@@ -25,6 +46,70 @@ public class User {
         this.birthDay = birthDay;
         this.phoneNumber = phoneNumber;
         this.status = status;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
